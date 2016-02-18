47dbde1d80c4cbc4a5737009a8c01ce700f0d982
Made Nindyatama <made.nindyatama@gmail.com>
2016-02-18 22:11:53 +0700

    Create WelcomeActivity

---
+++ WelcomeActivity.java
@@ -0,0 +1,56 @@
+package com.example.madenindya.hitcharide;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class WelcomeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        final Intent it_main = new Intent(this, MainActivity.class);
+
+        // set up the welcome timer
+        TimerTask ttask = new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(it_main);
+            }
+        };
+
+        Timer tr_welcomescreen = new Timer();
+        tr_welcomescreen.schedule(ttask, 5000);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_welcome, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
